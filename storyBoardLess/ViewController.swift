1ee7cb8c9a59bc81a9005f63bd5abe1e6e2d8456
yama-sho <shogo.yamamoto@ctc-g.co.jp>
2018-01-12 14:42:05 +0900

    Initial Commit

---
+++ storyBoardLess/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  storyBoardLess
+//
+//  Created by 山本省吾 on 2018/01/12.
+//  Copyright © 2018年 yama-sho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ storyBoardLessTests/storyBoardLessTests.swift
@@ -0,0 +1,36 @@
+//
+//  storyBoardLessTests.swift
+//  storyBoardLessTests
+//
+//  Created by 山本省吾 on 2018/01/12.
+//  Copyright © 2018年 yama-sho. All rights reserved.
+//
+
+import XCTest
+@testable import storyBoardLess
+
+class storyBoardLessTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
